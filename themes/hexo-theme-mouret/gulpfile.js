cfc5685698fe9fed2547ba2bf1ce4d24136b8e07
Sébastien Mouret <sebastien.mouret@gmail.com>
2017-05-05 16:50:42 +0200

    correction task gulp

--- themes/hexo-theme-mouret/gulpfile.js
+++ themes/hexo-theme-mouret/gulpfile.js
@@ -21,7 +21,7 @@ var banner = [
     ' * <%= pkg.name %> - <%= pkg.description %>',
     ' * @version v<%= pkg.version %>',
     ' * @homepage <%= pkg.homepage %>',
-    ' * @copyright ' + (new Date()).getFullYear() + ' <%= pkg.author.name %> ' ,
+    ' * @copyright ' + (new Date()).getFullYear() + ' <%= pkg.author.name %> ',
     ' * @license <%= pkg.license %>',
     ' */',
     '\n'
@@ -32,47 +32,47 @@ var files = {
     ],
     sass: ['source/sass/**/style.scss'],
     sassall: ['source/sass/**/*.scss'],
-    css:  [],
-    js:   [],
-    images:[],
-    svgs:  [],
+    css: [],
+    js: [],
+    images: [],
+    svgs: [],
     clean: [
     ]
 };
 // Compile Our Sass
-gulp.task('compileSass', function() {
+gulp.task('compileSass', function () {
     return gulp.src(files.sass)
-        .pipe(sass({style: 'expanded', quiet: true, cacheLocation: '.sass-cache'}))
+        .pipe(sass({ style: 'expanded', quiet: true, cacheLocation: '.sass-cache' }))
         .pipe(sass())
         .pipe(autoprefixer('last 1 version'))
-        .pipe(minifycss({keepSpecialComments: 0}))
+        .pipe(minifycss({ keepSpecialComments: 0 }))
         .pipe(rename('style.css'))
         .pipe(gulp.dest('source/css'));
 });
-gulp.task('css', function() {
+gulp.task('css', function () {
     runsequence(
         'compileSass');
 });
 // Concatenate & Minify JS
-gulp.task('js', function() {
+gulp.task('js', function () {
     return gulp.src(files.js)
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(header(banner, { pkg : pkg }))
+        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('source/js'));
 });
-gulp.task('concatJs', function() {
+gulp.task('concatJs', function () {
     return gulp.src(files.js)
         .pipe(concat('concat_scripts.js'))
         .pipe(gulp.dest('source/js'))
-        .pipe(header(banner, { pkg : pkg }))
+        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('source/js'));
 });
 // Images
-gulp.task('imgmin', function() {
+gulp.task('imgmin', function () {
     return gulp.src(files.images)
         .pipe(cache(imagemin({
             optimizationLevel: 3,
@@ -82,37 +82,37 @@ gulp.task('imgmin', function() {
         .pipe(gulp.dest('source/images'));
 });
 // Images
-gulp.task('svgmin', function() {
+gulp.task('svgmin', function () {
     return gulp.src(files.svgs)
         .pipe(cache(svgmin()))
         .pipe(gulp.dest('source/images'));
 });
 // Build
-gulp.task('build', function() {
-    return gulp.start(['compileSass','concatJs']);
+gulp.task('build', function () {
+    return gulp.start(['compileSass', 'concatJs']);
 });
 // Watch Files For Changes
-gulp.task('watch', ['default'], function() {
+gulp.task('watch', ['default'], function () {
     gulp.watch(files.lint, ['js']);
     gulp.watch(files.sassall, ['css']);
 });
 // Clean
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     files.clean.map(function (x) {
-        console.log("Deleting "+ x);
+        console.log("Deleting " + x);
     });
     return del(files.clean);
 });
 // Default Task
-gulp.task('default', function() {
-    gulp.start('lint', 'css', 'js', 'imgmin', 'svgmin');
+gulp.task('default', function () {
+    gulp.start('css', 'js', 'imgmin', 'svgmin');
 });
 gulp.task('zip', ['build'], function () {
     return gulp.src(['./**', '!node_modules/**'])
         .pipe(zip('hexo-theme-mouret.zip'))
         .pipe(gulp.dest('.'));
 });
-gulp.task('bump', function(){
+gulp.task('bump', function () {
     gulp.src('./package.json')
         .pipe(bump())
         .pipe(gulp.dest('./'));
